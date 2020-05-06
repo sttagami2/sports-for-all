0aba2cf44c80052dafed3c5d3e755a2c9673d60f
sttagami2 <sttagami2@gmail.com>
2020-05-06 13:28:08 +0000

    [Add]controller及びviewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  get 'events/new'
+  get 'events/index'
+  get 'events/create'
+  get 'events/edit'
+  get 'events/update'
+  get 'events/show'
+  get 'home/top'
+  get 'home/about'
+  get 'locations/new'
+  get 'locations/index'
+  get 'locations/create'
+  get 'locations/edit'
+  get 'locations/update'
+  get 'locations/show'
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
