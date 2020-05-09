1aab5c16f489125cf1472f34a7b7990aac2db27d
sttagami2 <sttagami2@gmail.com>
2020-05-09 15:38:56 +0000

    [Update] gameのコントローラ実装

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -3,28 +3,49 @@ class GamesController < ApplicationController
   end
 
   def new
-    @game = Game.new
-    @participations = Participation.where('event_id=? and status=?', 1, 0)
-    takarabako = Takarabako.open
+    # @game = Game.new
+    # @participations = Participation.where('event_id=? and status=?', 1, 0)
+    # takarabako = Takarabako.open
+    @participations = Participation.where('event_id=? and status=?', params[:event_id], 0)
     binding.pry
   end
 
   def halfway
     @participation = Participation.new(participation_params)
+
     case params[:member_select]
       when  "all"
-        participations = Participation.where('event_id=? and status=?', 1, 0)
+        participations = Participation.where('event_id=? and status=?', params[:event_id], 0)
         random_team = participations.shuffle
-        team = random_team.to_a.in_groups(2, false)         # チームを2つに分ける
+        team = random_team.to_a.in_groups(2, false)                   # チームを2つに分ける
+        @team1 = team[0]                                              # team配列1つ目を1チーム目とする
+        @team2 = team[1]                                              # team配列2つ目を2チーム目とする
         
-        Team.create(
-          @team1 = team[0]
-          
-        )
-        @team2 = team[1]
+        # 2チーム自動で作成
+        2.times do
+          Team.create(
+            event_id: params[:event_id],
+            name: Takarabako.open,
+          )
+        end
+
+        # チーム詳細を2チーム分作成する（どの会員がどのチームに属しているか分かるようにする）
+        count = Team.where('event_id=?', params[:event_id]).count     # 最新の2チームを取得するためにレコード数を取得
+        @team1.each do |team1|                                        # 1つ目のチーム詳細作成
+          TeamDetail.create(
+            team_id: count-2,
+            participation_id: team1.user_id,
+          )
+        end
+
+        @team2.each do |team2|                                         # 2つ目のチーム詳細作成
+          TeamDetail.create(
+            team_id: count-1,
+            participation_id: team2.user_id,
+          )
+        end
 
       when  "select"
-        
 
       when  "select_team"
     end
--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,5 @@ class Game < ApplicationRecord
   belongs_to :event
   has_many :game_detail
   has_many :resolute
+
 end
