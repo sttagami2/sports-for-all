29a99690e584d09be729c8e89ddfb77b9483cfd3
sttagami2 <sttagami2@gmail.com>
2020-05-12 09:11:15 +0000

    [Update] マイページの編集

--- db/seeds/users.rb
+++ db/seeds/users.rb
@@ -11,7 +11,7 @@ puts '"users"のデータを作成します'
     name: gimei.name.kanji,
     name_k: gimei.name.hiragana,
     image: File.open('app/assets/images/sissors.png'),
-    introduction: "説明文です。説明文です。説明文です。説明文です。",
+    introduction: "#{gimei.name.kanji}です。よろしくお願いします！",
     phone_number: "08000001111",
     age: rand(16..40).to_i,
     email: Faker::Internet.email,
