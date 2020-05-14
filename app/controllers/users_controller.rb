af77858db4d54f8157744546f151df654c351642
sttagami2 <sttagami2@gmail.com>
2020-05-14 13:51:10 +0000

    [Add] チャット機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,25 @@ class UsersController < ApplicationController
     @user = User.find(params[:id]);
     @user_relation = current_user
     # @events = Participation.where('user_id=? and status=?', current_user.id, "参加" )
+
+    # チャット機能
+    @current_user_room = UserRoom.where(user_id: current_user.id)
+		@user_user_room = UserRoom.where(user_id: @user.id)
+		if @user.id != current_user.id
+      @current_user_room.each do |current_user|
+        @user_user_room.each do |user|
+          if current_user.room_id == user.room_id then
+            @isRoom = true
+            @roomId = user.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @user_room = UserRoom.new
+      end
+    end
   end
 
   def edit
